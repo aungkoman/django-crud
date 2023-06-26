14623c14ada07603198cbe8aae6d1b774970416a
Aung Ko Man <aungkoman@gmail.com>
2023-06-26 14:21:49 +0630

    person obj list passed

--- finance/familytree/views.py
+++ finance/familytree/views.py
@@ -10,8 +10,9 @@ def index(request):
 def helloWorld(request):
     # html ဘက်ကို ထည့်ပေးချင်တဲ့ data
     person_list = ['Aung Aung', 'Ma Ma', 'Mg Mg']
+    person_obj_list = Person.objects.all()
     # ဖိုင်နာမည်နောက်မှာ Set တစ်ခု အနေနဲ့ ထည့်ပေး
-    return render(request, 'hello_world.html', {'person_list': person_list, 'title' : 'Person List' } )
+    return render(request, 'hello_world.html', {'person_list': person_list, 'title' : 'Person List', 'person_obj_list' : person_obj_list } )
 
 def personDetail(request, person_id = 0):
     data = {
