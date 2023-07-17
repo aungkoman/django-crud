1f6b07b31b9a30d368cfd5429d24cc36ca44fe81
Aung Ko Man <aungkoman@gmail.com>
2023-07-17 18:44:19 +0630

    link url in project for newseleven

--- finance/finance/urls.py
+++ finance/finance/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("familytree.urls")),
+    path("", include("newseleven.urls")),
+    path("/family", include("familytree.urls")),
     path("money/", include("money.urls")),
     path("admin/", admin.site.urls),
 ]
