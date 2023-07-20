62d903f5baad5ecd808ce566aa538c04e95ce2bf
Aung Ko Man <aungkoman@gmail.com>
2023-07-20 15:33:45 +0630

    image upload ok

--- finance/finance/urls.py
+++ finance/finance/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("", include("newseleven.urls")),
-    path("/family", include("familytree.urls")),
+    path("family/", include("familytree.urls")),
     path("money/", include("money.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
